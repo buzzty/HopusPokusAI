d8c23be405f230d21e0d0c3cb5e22a157f2c9ce1
Nico <nicolas.ruderisch@gmail.com>
2021-03-25 20:55:27 +0100

    worked on boss in a test scene

---
+++ PW_SoSe_AI/Assets/Scripts_Hopus-Pocus/HopusPocus/HPEnemyBrain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using AISystem.HopusPocus;
+using UnityEngine;
+
+namespace AISystem
+{
+    /// <summary>
+    /// 	Class that hooks up an enemy with its state FSM
+    /// </summary>
+    public class HPEnemyBrain : MonoBehaviour
+    {
+        [SerializeField] private HPEnemyPhaseFSM _phaseFSM = default;
+        [SerializeField] private Enemy _enemy;
+
+        private void Start()
+        {
+            _phaseFSM.Init(_enemy);
+        }
+
+        private void Update()
+        {
+            // update the phase FSM
+            _phaseFSM.Tick(_enemy);
+        }
+    }
+}
